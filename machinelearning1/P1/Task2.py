7f6112d315947e87791ce340df3ce35ca9acd20a
edwardpan <pan_61@126.com>
2018-01-25 23:58:25 +0800

    complete task0-task3

--- machinelearning1/P1/Task2.py
+++ machinelearning1/P1/Task2.py
@@ -22,4 +22,25 @@ September 2016.".
 如果键已经存在于字典内，为键所对应的值加上对应数值；
 如果键不存在于字典内，将此键加入字典，并将它的值设为给定值。
 """
+total_times = {}
+max_second = 0
+max_second_phone = ""
+for call in calls:
+    call_second = int(call[3])
 
+    call_from_time = total_times.get(call[0], 0)
+    call_from_time_new = call_from_time + call_second
+    total_times[call[0]] = call_from_time_new
+    if call_from_time_new > max_second:
+        max_second = call_from_time_new
+        max_second_phone = call[0]
+
+    call_to_time = total_times.get(call[1], 0)
+    call_to_time_new = call_to_time + call_second
+    total_times[call[1]] = call_to_time_new
+    if call_to_time_new > max_second:
+        max_second = call_to_time_new
+        max_second_phone = call[1]
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016."
+      .format(max_second_phone, max_second))
--- machinelearning1/P1/Task3.py
+++ machinelearning1/P1/Task3.py
@@ -39,3 +39,91 @@ with open('calls.csv', 'r') as f:
 to other fixed lines in Bangalore."
 注意：百分比应包含2位小数。
 """
+
+def is_bangalore_phone(phone):
+    """判断是否是班加罗尔的固定电话
+    :param phone:电话号码,字符串
+    :return: True:是班加罗尔的固定电话,False:不是
+    """
+    return phone[:5] == "(080)"
+
+def is_fixed_phone(phone):
+    """判断是否是固定电话
+    :param phone: 电话号码,字符串
+    :return: True:是固定电话,False:不是
+    """
+    return "(" in phone and ")" in phone
+
+def is_saler_phone(phone):
+    """判断是否是电话促销员的号码
+    :param phone: 电话号码,字符串
+    :return: True:是电话促销员,False:不是
+    """
+    return str(phone).startswith("140")
+
+def is_mobile_phone(phone):
+    """判断是否是移动号码
+    :param phone: 电话号码,字符串
+    :return: True:是移动号码,False:不是
+    """
+    return not is_fixed_phone(phone) and " " in phone
+
+def get_area_code(phone):
+    """获取固定电话的区号
+    :param phone: 电话号码,字符串
+    :return: 区号
+    """
+    if is_fixed_phone(phone):
+        end_index = phone.index(")")
+        return phone[1:end_index]
+    else:
+        raise ValueError("not a fixed phone, no area code")
+
+def get_mobile_phone_tag(phone):
+    """获取移动号码的前缀
+    :param phone: 电话号码,字符串
+    :return: 移动号码前缀
+    """
+    if is_mobile_phone(phone):
+        return phone[:4]
+    else:
+        raise ValueError("not a mobile phone, no tag")
+
+def find_phone():
+    """找出被班加罗尔地区的固定电话所拨打的所有电话的区号和移动前缀"""
+    code_or_tags = set()
+    for call in calls:
+        from_phone = call[0]
+        to_phone = call[1]
+        if is_bangalore_phone(from_phone): # 主叫号码是班加罗尔地区
+            if is_fixed_phone(to_phone): # 被叫是固定号码
+                code_or_tags.add(get_area_code(to_phone))
+            elif is_mobile_phone(to_phone): # 被叫是移动号码
+                code_or_tags.add(get_mobile_phone_tag(to_phone))
+    code_tags_list = list(code_or_tags)
+    code_tags_list.sort()
+    return code_tags_list
+
+print("The numbers called by people in Bangalore have codes:")
+phones = find_phone()
+for phone in phones:
+    print(phone)
+
+def find_rate():
+    """计算由班加罗尔固话打往班加罗尔固话在班加罗尔固话打出的所有号码中所占比例"""
+    bangalore_phone = set()
+    total_phone = set()
+    for call in calls:
+        from_phone = call[0]
+        to_phone = call[1]
+        if is_bangalore_phone(from_phone): # 主叫号码是班加罗尔地区
+            total_phone.add(to_phone)
+            if is_bangalore_phone(to_phone): # 被叫号码是班加罗尔地区
+                bangalore_phone.add(to_phone)
+    return round(len(bangalore_phone) / len(total_phone) * 100, 2)
+
+print("{} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore."
+      .format(find_rate()))
+
+
+
