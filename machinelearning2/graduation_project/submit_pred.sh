dedc4cc4085fcaa3b063055beb15514a201f2f26
edwardpan <edwardp.61@gmail.com>
2018-06-25 08:11:08 +0000

    update project

--- machinelearning2/graduation_project/submit_pred.sh
+++ machinelearning2/graduation_project/submit_pred.sh
@@ -1,1 +1,1 @@
-kaggle competitions submit -c state-farm-distracted-driver-detection -f data/pred.csv -m "test"
+kaggle competitions submit -c state-farm-distracted-driver-detection -f data/pred.csv -m "$1"
